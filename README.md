ffbc9649dfd8d6665a25ea0e86173545a1b1afb1
Daniel <mwdchang@gmail.com>
2020-07-26 11:50:18 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Song Tiles
+Automatocially composing an image mosaic using albumg cover arts.
+
+Image data manipulation done via my image utilities in https://github.com/mwdchang/image-util
+
+### Customization
+- Replace imageURLs in images.js, these are the image to be used as the mosaic tiles
+- Replace names/imageURLs in source-images, these are the images we want to tile
+
+### Notes/TODO
+For easy of calculation, images are fiitted to square dimensions.
+
+### Credits
+Sample images used were taken from Spotify API and Pexels
---
+++ source-images.js
@@ -0,0 +1,7 @@
+// Example images taken from www.pexels.com
+const sourceImages = [
+  { name: 'Flower', imageURL: 'https://images.pexels.com/photos/850359/pexels-photo-850359.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' },
+  { name: 'Guitar', imageURL: 'https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
+  { name: 'Lightning', imageURL: 'https://images.pexels.com/photos/258173/pexels-photo-258173.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
+  { name: 'Tea', imageURL: 'https://images.pexels.com/photos/1417945/pexels-photo-1417945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
+];
